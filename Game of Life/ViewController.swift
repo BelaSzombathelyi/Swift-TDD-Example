5c8159980b19d58e52743dedc9489ab8a24c4716
Béla Szombathelyi <szbbb2+GitHub@gmail.com>
2018-11-01 14:22:22 +0100

    First commit

---
+++ Game of Life/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Game of Life
+//
+//  Created by Béla Szomathelyi on 2018. 11. 01..
+//  Copyright © 2018. Béla Szomathelyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+}
+
