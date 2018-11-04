fe587f1e60435bee0fb43e2b06dd6156135c76a0
Béla Szombathelyi <szbbb2+GitHub@gmail.com>
2018-11-04 12:01:35 +0100

    [UPD] GameInteractorSpec & Universe Extension

---
+++ Game of LifeTests/Scenes/Game/GameInteractorSpec.swift
@@ -0,0 +1,10 @@
+import Quick
+import Nimble
+@testable import Game_of_Life
+
+class GameInteractorSpec: QuickSpec {
+
+   override func spec() {
+      
+   }
+}
--- Game of LifeTests/Scenes/Game/GameWorkerSpec.swift
+++ Game of LifeTests/Scenes/Game/GameWorkerSpec.swift
@@ -2,31 +2,6 @@ import Quick
 import Nimble
 @testable import Game_of_Life
 
-extension Universe {
-   
-   fileprivate static func cleanUniverse(withDimensions dimensions: Dimensions, _ editCloser: (_ dimensions: Dimensions, _ cells: inout [[Bool]]) -> Void = { _, _ in }) -> Universe {
-      var table = [[Bool]]()
-      for _ in 0..<dimensions.height {
-         let row = Array(repeating: false, count: Int(dimensions.width))
-         table.append(row)
-      }
-      editCloser(dimensions, &table)
-      return Universe(dimensions: dimensions, cells: table)
-   }
-   
-   func count(ofValue value: Bool) -> Int {
-      var counter = 0
-      for row in 0..<self.cells.count {
-         for column in 0..<self.cells[row].count {
-            if (self.cells[row][column] == value) {
-               counter += 1
-            }
-         }
-      }
-      return counter
-   }
-}
-
 class GameWorkerSpec: QuickSpec {
    
    override func spec() {
